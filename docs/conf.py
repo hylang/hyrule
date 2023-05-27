482ff36d8686c766befad374a0a4b222e2d962d1
Sunjay Cauligi <scauligi@eng.ucsd.edu>
2023-05-27 22:09:51 +0200

    set default code highlighting language in sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@ exclude_patterns = ['_build']
 smartquotes = False
 nitpicky = True
 
+highlight_language = "hylang"
+
 intersphinx_mapping = dict(
     py = ('https://docs.python.org/3/', None),
     hy = ('https://docs.hylang.org/en/master', None))
